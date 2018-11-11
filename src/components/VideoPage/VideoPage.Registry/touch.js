0a84552044cb914a8eacfddb5ba07bfa9eb19d11
TheNech <savchenkov.vk@yandex.ru>
2018-11-12 00:12:07 +0300

    исправлена опечатка в VideoPage.Registry/touch

--- src/components/VideoPage/VideoPage.Registry/touch.js
+++ src/components/VideoPage/VideoPage.Registry/touch.js
@@ -3,7 +3,7 @@ import { Registry } from '@bem-react/di';
 import { Header, cnHeader } from '../../Header/Header@touch';
 import { Video, cnVideo } from '../../Video/Video@touch';
 
-export const desktop = new Registry({ id: 'platform' });
+export const touch = new Registry({ id: 'platform' });
 
-desktop.set(cnHeader(), Header);
-desktop.set(cnVideo(),  Video);
+touch.set(cnHeader(), Header);
+touch.set(cnVideo(),  Video);
